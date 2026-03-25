39bb13443c9af2ef58a29e8d0dc65a1d6783b650
nyotaloansfundingcoke-lgtm <nyotaloansfunding.co.ke@gmail.com>
2026-03-25 11:29:52 +0300

    Update loan.ts

--- src/types/loan.ts
+++ src/types/loan.ts
@@ -18,20 +18,20 @@ export interface PaymentResponse {
 }
 
 export const LOAN_OPTIONS: LoanOption[] = [
-  { amount: 5500, fee: 100 },
-  { amount: 6800, fee: 130 },
-  { amount: 7800, fee: 170 },
-  { amount: 9800, fee: 190 },
-  { amount: 11200, fee: 230 },
-  { amount: 16800, fee: 250 },
-  { amount: 21200, fee: 270 },
-  { amount: 25600, fee: 400 },
-  { amount: 30000, fee: 470 },
-  { amount: 35400, fee: 590 },
-  { amount: 39800, fee: 730 },
-  { amount: 44200, fee: 1010 },
-  { amount: 48600, fee: 1600 },
-  { amount: 60600, fee: 2050 },
+  { amount: 5500, fee: 199 },
+  { amount: 6800, fee: 229 },
+  { amount: 7800, fee: 269 },
+  { amount: 9800, fee: 289 },
+  { amount: 11200, fee: 329 },
+  { amount: 16800, fee: 349 },
+  { amount: 21200, fee: 369 },
+  { amount: 25600, fee: 499 },
+  { amount: 30000, fee: 569 },
+  { amount: 35400, fee: 689 },
+  { amount: 39800, fee: 829 },
+  { amount: 44200, fee: 1109 },
+  { amount: 48600, fee: 1699 },
+  { amount: 60600, fee: 2149 },
 ];
 
 export const LOAN_TYPES = [
